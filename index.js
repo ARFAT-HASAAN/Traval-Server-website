9fcd795d3fdde0fe65257c13f4085606915a5c51
ARFAT-HASAAN <arfuhtv757@gmail.com>
2021-11-04 09:21:35 +0600

    ami tomaro rohibo bilono tomate kiribo bash

--- index.js
+++ index.js
@@ -3,50 +3,60 @@ const cors = require('cors')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const { MongoClient } = require('mongodb');
-const app = express()
-const port = 2000
+const ObjectId = require('mongodb').ObjectId
 
+const port = process.env.PORT || 2000
+const app = express()
 // middle wire 
 app.use(bodyParser.json())
 app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m9s95.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+console.log(uri)
 const client = new MongoClient(uri,
-     {
-     useNewUrlParser: true,
+   {
+      useNewUrlParser: true,
       useUnifiedTopology: true
-       });
-
-
-async function run(run) {
-    try{
-
-    
- await client.connect();
-    console.log(('did you connect with database?'));
-    // const database = client.db("insertDB");
-    // const haiku = database.collection("haiku");
-    // create a document to insert
-   
-    // const result = await haiku.insertOne(doc);
-
-    // console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    }
- finally{
-
-    //  await client.close()
- }
-    
-}
- run().catch(console.dir)
-
-
+   });
 
 app.get('/', (req, res) => {
-  res.send('assignment 11')
+   res.send('assignment 11')
 })
 
+async function run() {
+   try {
+      await client.connect();
+
+      const database = client.db("travel_service");
+      const servicedata = database.collection("service");
+
+      // get api 
+      app.get('/services', async (req, res) => {
+         const service = await servicedata.find({}).toArray()
+
+
+         res.send(service)
+
+      })
+
+      // post api 
+      app.post('/services', async (req, res) => {
+
+         const newService = req.body;
+         const result = await servicedata.insertOne(newService)
+         res.send(result)
+
+
+      })
+
+   } finally {
+      // await client.close();
+   }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-  console.log('my port number is', port)
+   console.log('my port number is', port)
 })
